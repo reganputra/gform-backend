29c58eb66614365439abf9a7ff406308893ba3fb
reganputra <ramadhanregan334@gmail.com>
2024-05-17 08:43:53 +0700

    adding route for question

--- routes/api.js
+++ routes/api.js
@@ -19,11 +19,12 @@ router.get("/forms/:id", jwtAuth(), FormController.show);
 router.put("/forms/:id", jwtAuth(), FormController.update);
 router.delete("/forms/:id", jwtAuth(), FormController.destroy);
 
-// router.get("/forms", jwtAuth(), FormController.index);
+router.get("/forms/:id/questions", jwtAuth(), QuestionController.index);
 router.post("/forms/:id/questions", jwtAuth(), QuestionController.store);
 // router.get("/forms/:id", jwtAuth(), FormController.show);
 router.put("/forms/:id/questions/:questionId", jwtAuth(), QuestionController.update);
-// router.delete("/forms/:id", jwtAuth(), FormController.destroy);
+router.delete("/forms/:id/questions/:questionId", jwtAuth(), QuestionController.destroy);
+
 
 
 
