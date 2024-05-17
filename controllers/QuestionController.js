bbb7d606c0edb5aa3222a8ef814768e064c46228
reganputra <ramadhanregan334@gmail.com>
2024-05-17 07:52:45 +0700

    fix update at QuestionController

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -2,69 +2,88 @@ import mongoose from "mongoose";
 import Form from "../models/Form.js";
 
 class QuestionController {
-
-    // Add Question
     async store(req, res) {
         try {
-            if(!req.params.id) {throw {code: 400, message: "Required_Fomr_ID"}}
-            if(!mongoose.Types.ObjectId.isValid(req.params.id)) {throw {code: 400, message: "Required_Form_ID"}}
+            //check form id
+            if(!req.params.id) { throw { code: 428, message: "ID_REQUIRED" } }
+            if(!mongoose.Types.ObjectId.isValid(req.params.id)) { throw { code: 400, message: "INVALID_ID" } }
 
-            const newQuestion = {
-                _id: new mongoose.Types.ObjectId(),
-                questions: null,
-                type: "Text",
+            //input field
+            let newQuestion = {
+                id: new mongoose.Types.ObjectId(),
+                type: 'Text',
+                question: null,
+                options: [],
                 required: false,
-                option: []
             }
 
-            const form = await Form.findOneAndUpdate({_id: req.params.id, userId: req.jwt.id}, {$push: {questions: newQuestion}}, {new: true})
-            if(!form) {throw {code: 400, message: "Form not found"}}
+            //update form
+            const question = await Form.findOneAndUpdate(
+                                        { _id: req.params.id, userId: req.jwt.id }, 
+                                        { $push: { questions: newQuestion } }, 
+                                        { new: true })
 
-            return res.status(200)
-            .json({
-                status: true,
-                message: "Question added successfully",
-                question: newQuestion
-            })
+            if(!question) { throw { code: 500, message: "ADD_QUESTION_FAILED" } }
+
+            res.status(200).json({
+                    status: true,
+                    message: 'ADD_QUESTION_SUCCESS',
+                    question: newQuestion
+                })
         } catch (error) {
-            return res.status(error.code || 500).json({
-                status: false,
-                message: error.message,
-
-            })
+            res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message,
+                })
         }
     }
 
-    // Update Question
     async update(req, res) {
         try {
-            if(!req.params.id) {throw{code: 400, message: "Required_Form_ID"}}
-            if(!req.params.questionId) {throw{code: 400, message: "Required_Question_ID"}}
-            if(!mongoose.Types.ObjectId.isValid(req.params.id)) {throw{code: 400, message: "Required_Question_ID"}}
-            if(!mongoose.Types.ObjectId.isValid(req.params.questionId)) {throw{code: 400, message: "Invalid_ID"}}
+            //check form id
+            if(!req.params.id) { throw { code: 428, message: "FORM_ID_REQUIRED" } }
+            if(!req.params.questionId) { throw { code: 428, message: "QUESTION_ID_REQUIRED" } }
+            if(!mongoose.Types.ObjectId.isValid(req.params.id)) { throw { code: 400, message: "INVALID_ID" } }
 
+            let field = {}
+            if(req.body.hasOwnProperty('question')){
+                field['questions.$[indexQuestion].question'] = req.body.question
+            }
+            else if(req.body.hasOwnProperty('required')){
+                field['questions.$[indexQuestion].required'] = req.body.required
+            }
+            else if(req.body.hasOwnProperty('type')){
+                if(!allowedTypes.includes(req.body.type)) { throw { code: 400, message: "INVALID_TYPE" } }
+                field['questions.$[indexQuestion].type'] = req.body.type
+            }
+
+            //update form
             const question = await Form.findOneAndUpdate(
-                { _id: req.params.id, userId: req.jwt.id},
-                 {$set: {'questions.$[indexQuestion.id].question': req.body.question}},
-                {arrayFilters: [{'indexQuestion.id':  mongoose.Types.ObjectId(req.params.questionId)}], new:true }
-                )
-            
-                if(!form) {throw {code: 400, message: "Question_Update_Failed"}}
+                                        { _id: req.params.id, userId: req.jwt.id }, 
+                                        { $set : field },
+                                        { 
+                                            arrayFilters: [ { 'indexQuestion.id': new mongoose.Types.ObjectId(req.params.questionId) } ],
+                                            new: true 
+                                        })
 
-            return res.status(200).json({
-                status: true,
-                message: "Question updated successfully",
-                question
-            })
+            if(!question) { throw { code: 500, message: "UPDATE_QUESTION_FAILED" } }
+
+            res.status(200).json({
+                    status: true,
+                    message: 'UPDATE_QUESTION_SUCCESS',
+                    question
+                })
         } catch (error) {
-            return res.status(error.code || 500).json({
-                status: false,
-                message: error.message,
-
-            })
+            res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message,
+                })
         }
     }
 }
+
 
 export default new QuestionController();
 
