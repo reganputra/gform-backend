26b5d983af960fd11da37ee81479b23d2b086a08
reganputra <ramadhanregan334@gmail.com>
2024-07-16 08:51:40 +0700

    add fakerjs

---
+++ faker/answer.js
@@ -0,0 +1,18 @@
+import  connection  from "../connection.js";
+import {faker} from "@faker-js/faker"
+import Answer from "../models/Answer.js"
+
+connection()
+
+let data = []
+for(let i=0; i<20; i++){
+    data.push({
+        "6695cf5189a3dd563684b328": faker.person.fullName(),
+        "6684a8cc478516e9586b867c": faker.helpers.arrayElement(["53", "20"]),
+        "668b481354a7069f96e844ed": faker.helpers.arrayElements(["Soto", "Rawon", "Bakso"]),
+        "formId": "6684a8bd478516e9586b867a",
+        "userId": "6684b205ff39f75504c1319c"
+    })
+}
+
+Answer.insertMany(data)
