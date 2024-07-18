edee1c356e442d980e5cc0694a6f2efda4112a01
reganputra <ramadhanregan334@gmail.com>
2024-07-18 08:42:23 +0700

    adding response module

---
+++ controllers/ResponseController.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import Form from "../models/Form.js";
+import Answer from "../models/Answer.js";
+
+
+class ResponseController {
+
+    async lists(req, res) {
+        try {
+            if(!req.params.formId){throw{code: 400, message: "Required_Form_Id"}}
+            if(!mongoose.Types.ObjectId.isValid(req.params.formId)) {throw{code: 400, message: "Invalid_Id"}}
+
+            const form = await Form.findOne({_id: req.params.formId, userId: req.jwt.id}).populate("answers")
+            if(!form) {throw{code: 400, message: "Form_Not_Found"}}
+
+            return res.status(200)
+                    .json({
+                        status: true,
+                        message: "Answer_Found",
+                        form,
+                        total: form.answers.length,
+                        answers: form.answers
+                    })
+
+        } catch (error) {
+            res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message
+                })
+        }
+    }
+
+    async summaries(req, res) {
+        try {
+            if(!req.params.formId){throw{code: 400, message: "Required_Form_Id"}}
+            if(!mongoose.Types.ObjectId.isValid(req.params.formId)) {throw{code: 400, message: "Invalid_Id"}}
+
+            const form = await Form.findOne({_id: req.params.formId, userId: req.jwt.id}).populate("answers")
+            if(!form) {throw{code: 400, message: "Form_Not_Found"}}
+
+            const summaries = form.questions.map(question => {
+                const summary = {
+                    type: question.type,
+                    questionId: question.id,
+                    question: question.question,
+                    answers: form.answers.map((answer) => answer[question.id])
+                }
+                return summary
+            })
+
+
+            return res.status(200)
+                    .json({
+                        status: true,
+                        message: "Answer_Found",
+                        summaries
+                    })
+
+        } catch (error) {
+            res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message
+                })
+        }
+    }
+}
+
+export default new ResponseController()
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ import QuestionController from "../controllers/QuestionController.js";
 import OptionController from "../controllers/OptionController.js";
 import AnswerController from "../controllers/AnswerController.js";
 import InviteController from "../controllers/InviteController.js";
+import ResponseController from "../controllers/ResponseController.js";
 
 
 const router = express.Router();
@@ -41,5 +42,10 @@ router.post("/answers/:formId", jwtAuth(), AnswerController.store);
 router.get("/forms/:id/invites", jwtAuth(), InviteController.index);
 router.post("/forms/:id/invites", jwtAuth(), InviteController.store);
 router.delete("/forms/:id/invites", jwtAuth(), InviteController.destroy);
+
+// Response
+router.get("/responses/:formId/lists", jwtAuth(), ResponseController.lists);
+router.get("/responses/:formId/summaries", jwtAuth(), ResponseController.summaries);
+
 
 export default router;
