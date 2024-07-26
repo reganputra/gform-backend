68232974ecbc1c434f2025f55f38631948abc8c5
reganputra <ramadhanregan334@gmail.com>
2024-07-26 09:22:51 +0700

    install cors and register frontend localhost to backend

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import apiRouter from './routes/api.js'
 import connection from './connection.js'
+import cors from 'cors'
 
 const app = express()
 const env = dotenv.config().parsed
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
+app.use(cors({origin:'http://localhost:8000'}))
 
 app.use('/', apiRouter)
 
